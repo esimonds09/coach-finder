3e44ccd14d23710bba13c7198e925657621bb573
Eric S <esimonds09@gmail.com>
2023-12-26 15:57:26 -0500

    Wire pages

---
+++ src/pages/coaches/CoachRegister.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>REGISTER</section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>CONTACT A COACH</section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
