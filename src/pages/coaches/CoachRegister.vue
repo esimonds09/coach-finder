a346c8c4711367d4c83d8014009f19e27b4526ea
Eric S <esimonds09@gmail.com>
2023-12-27 18:05:58 -0500

    create CoachForm.vue to register coach

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,9 +1,20 @@
 <template>
-  <section>REGISTER</section>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
 </script>
 
 <style></style>
