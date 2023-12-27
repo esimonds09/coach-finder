e7ba07b9c81161afd20fdac543d756d75e4f9083
Eric S <esimonds09@gmail.com>
2023-12-27 17:41:55 -0500

    add coach filter component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <section>FILTER</section>
-    <base-card>
-      <section>
+    <section>
+      <coach-filter @change-filter="setFilters"></coach-filter>
+    </section>
+    <section>
+      <base-card>
         <div class="controls">
           <base-button mode="outline">Refresh</base-button>
           <base-button link to="/register">Register as Coach</base-button>
@@ -19,26 +21,53 @@
           ></coach-item>
         </ul>
         <h3 v-else>No coaches found.</h3>
-      </section>
-    </base-card>
+      </base-card>
+    </section>
   </div>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
 import BaseButton from '../../components/ui/BaseButton.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default {
   components: {
     CoachItem,
     BaseButton,
+    CoachFilter,
+  },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
   },
   computed: {
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches'];
+      const coaches = this.$store.getters['coaches/coaches'];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
+        }
+      });
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
     },
   },
 };
